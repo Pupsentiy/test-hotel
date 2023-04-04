ea1823124479041e9bff5e00545117b07aa0c38b
Alexander <pupsentiy@gmail.com>
2023-04-04 22:58:49 +0300

    текущий коммит

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 export const fetchHotelsFromApi = async () => {
   try {
-    const res = await fetch(`http://engine.hotellook.com/api/v2/cache.json?location=Moscow&currency=rub&checkIn=&checkOut=2023-04-06&limit=20`)
+    const res = await fetch(`http://engine.hotellook.com/api/v2/cache.json?location=Moscow&currency=rub&checkIn=2023-04-04&checkOut=2023-04-06&limit=20`)
     const data = await res.json()
     console.log(data,'fetchHoteldata')
     return data
